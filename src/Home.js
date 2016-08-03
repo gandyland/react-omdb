1fbd7ddc14f989541cdce7d9b35a3e4941865d90
Andrew Young <prince.of.utah@gmail.com>
2016-08-03 11:43:37 -0400

    added SearchContainer.js

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,13 @@
 // // Developer TODO: Define App component defintion here
 import React, {Component} from 'react'
-import Search from "./Search"
+import SearchContainer from "./SearchContainer"
 
 class Hello extends Component {
   render () {
     return (
       <div>
         <h1>OMDB React</h1>
-        <Search />
+        <SearchContainer />
       </div>
     );
   }
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,32 @@
 import React, {Component} from "react"
 
 class Search extends Component {
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     query: ''
+  //   }
+  // }
+  // handleSearchInput (e) {
+  //   this.setState({
+  //     query: e.target.value
+  //   })
+  // }
+  // submitQuery(evt){
+  //   evt.preventDefault();
+  //   console.log(this.state.query)
+  // }
+
   render(){
+    let {handleSearchInput, handleSubmitQuery, query} = this.props
     return (
       <div className="searchBox">
-        <form>
-          <input type="text" placeholder="Enter a Movie Title..." />
+        <form onSubmit={(evt) => handleSubmitQuery(evt)}>
+          <input
+            onChange={(evt) => handleSearchInput(evt)}
+            value={query}
+            type="text"
+            placeholder="Enter a Movie Title..." />
           <button type="submit">Search</button>
         </form>
       </div>
